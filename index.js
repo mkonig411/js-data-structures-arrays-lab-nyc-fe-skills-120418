db0c8803238e58195d5a4d3e7cce2085102dcc00
Margot Konig <mkonig411@gmail.com>
2019-01-23 00:36:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const drivers = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendDriver (name) {
   drivers.push(name) 
 }
+function destructivelyPrependDriver (name){
+  drivers.pop(name)
+}
